738a09c46a82b53a9d19c025a1e021cc62d7346e
Captain Iceboy <richard21@ru.is>
2022-05-03 15:43:56 +0000

    Added side navbar

--- BELL/BELL/urls.py
+++ BELL/BELL/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+    path('', include('frontpage.urls')),
     path('admin/', admin.site.urls),
     path('frontpage', include('frontpage.urls')),
 ]
