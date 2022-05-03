0ed3a8da0b3e7df8b8476c2270fd0a61f0ad6348
ezithau <ezithau@gmail.com>
2022-05-03 15:55:20 +0000

    sql file correction

--- BELL/Data/tabledll.sql
+++ BELL/Data/tabledll.sql
@@ -5,42 +5,42 @@ drop table if exists people;
 drop table if exists location;
 
 create table location (
-	zip integer not null;
-	lname varchar(50) not null;
+	zip int not null,
+	lname varchar(50) not null,
 	primary key(zip)
 );
 
 create table people (
-	uid integer not null;
-	pname varchar(100);
-	zip integer;
-	primary key(uid);
+	uid integer not null,
+	pname varchar(100),
+	zip integer,
+	primary key(uid),
 	foreign key(zip) references location(zip)
  );
 
 create table users (
-	uid integer not null;
-	uname varchar(15) not null;
-	email varchar(50) not null;
-	password varchar(200) not null;
-	primary key(uid, uname, email);
-	foreign key(uid) references people(uid);
-);
-	
-create table items (
-	iid integer not null;
-	uid integer not null;
-	startbid float not null;
-	condition varchar(20);
-	description varchar(350);
-	primary key(iid);
-	foreign key(uid) references people(uid);
+	uid integer not null,
+	uname varchar(15) not null,
+	email varchar(50) not null,
+	password varchar(200) not null,
+	primary key(uid, uname, email),
+	foreign key(uid) references people(uid)
 );
 
-create table sales ( 
-	iid integer not null;
-	sell_price float not null;
-	highest_bid float not null;
-	primary key(iid);
-	foreign key(iid) references items(iid);
+create table items (
+	iid integer not null,
+	uid integer not null,
+	startbid float not null,
+	condition varchar(20),
+	description varchar(350),
+	primary key(iid),
+	foreign key(uid) references people(uid)
+);
+
+create table sales (
+	iid integer not null,
+	sell_price float not null,
+	highest_bid float not null,
+	primary key(iid),
+	foreign key(iid) references items(iid)
 );
