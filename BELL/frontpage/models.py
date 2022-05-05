e38c990b6b850c210c1b6e0bffba2668972d902a
Hjölli <ezithau@gmail.com>
2022-05-05 21:25:39 +0000

    models set up

--- BELL/frontpage/models.py
+++ BELL/frontpage/models.py
@@ -1,19 +1,33 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+
+class Locations(models.Model):
+    zip = models.IntegerField(unique=True)
+    name = models.CharField(max_length=255)
+
+
+class People(models.Model):
+    pname = models.CharField(max_length=255)
+    location = models.ForeignKey(Locations, default=0, on_delete=models.SET_DEFAULT)
 
 
 class Users(models.Model):
+    uuid = models.ForeignKey(People, on_delete=models.CASCADE)
     user_name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)
+    password = models.CharField(max_length=9999, blank=True)
 
+
+class ItemImage(models.Model):
+    image = models.CharField(max_length=9999, blank=False,default=0)
 
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
     user_rank = models.CharField(max_length=255, blank=True)
     starting_price = models.FloatField()
+    first_image = models.ForeignKey(ItemImage, default=0, on_delete=models.SET_DEFAULT)
+    images = ArrayField(models.IntegerField())
 
-
-class ItemImage(models.Model):
-    image = models.CharField(max_length=9999)
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
 
--- BELL/search/views.py
+++ BELL/search/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import render
 def index(request):
     if request.method == "POST":
         searched = request.POST['searched']
-        items =
         return render(request, 'search/index.html', {'searched': searched})
     else:
         return render(request, 'search/index.html')
