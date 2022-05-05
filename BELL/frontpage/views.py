a251ffe768e7f932f5d1de5d47c70f269fd2cfd0
Captain Iceboy <richard21@ru.is>
2022-05-05 11:09:52 +0000

    Changed frontpage html

--- BELL/frontpage/views.py
+++ BELL/frontpage/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+frontpage_suggested = [
+    {'image': 'https://d27jswm5an3efw.cloudfront.net/app/uploads/2019/07/insert-image-html.jpg', 'name': 'something',
+     'rating': 'unknown', 'highest': '10'},
+    {'image': 'https://d27jswm5an3efw.cloudfront.net/app/uploads/2019/07/insert-image-html.jpg', 'name': 'something',
+     'rating': 'unknown', 'highest': '10'},
+    {'image': 'https://d27jswm5an3efw.cloudfront.net/app/uploads/2019/07/insert-image-html.jpg', 'name': 'something',
+     'rating': 'unknown', 'highest': '10'},
+    {'image': 'https://d27jswm5an3efw.cloudfront.net/app/uploads/2019/07/insert-image-html.jpg', 'name': 'something',
+     'rating': 'unknown', 'highest': '10'},
+    {'image': 'https://d27jswm5an3efw.cloudfront.net/app/uploads/2019/07/insert-image-html.jpg', 'name': 'something',
+     'rating': 'unknown', 'highest': '10'},
+    {'image': 'https://d27jswm5an3efw.cloudfront.net/app/uploads/2019/07/insert-image-html.jpg', 'name': 'something',
+     'rating': 'unknown', 'highest': '10'},
+    {'image': 'https://d27jswm5an3efw.cloudfront.net/app/uploads/2019/07/insert-image-html.jpg', 'name': 'something',
+     'rating': 'unknown', 'highest': '10'},
+    {'image': 'https://d27jswm5an3efw.cloudfront.net/app/uploads/2019/07/insert-image-html.jpg', 'name': 'something',
+     'rating': 'unknown', 'highest': '10'}
+]
 
 
 def index(request):
-    return render(request, 'frontpage/index.html')
+    return render(request, 'frontpage/index.html', context={'frontpage_suggested': frontpage_suggested})
