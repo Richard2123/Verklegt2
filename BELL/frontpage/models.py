8c8cf920dec777dfb5ec0179d46e200e640c64c5
Captain Iceboy <richard21@ru.is>
2022-05-05 12:57:45 +0000

    Added some classes to frontpage models

--- BELL/frontpage/models.py
+++ BELL/frontpage/models.py
@@ -2,3 +2,15 @@ from django.db import models
 
 class Users(models.Model):
     user = models.CharField(max_length=255)
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    user_rank = models.CharField(max_length=255, blank=True)
+    price = models.FloatField()
+
+
+class ItemImage(models.Model):
+    image = models.CharField(max_length=9999)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
