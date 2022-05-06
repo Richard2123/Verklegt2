43d1f6819bfbaa652c6092684876812a24646b4f
ezithau <ezithau@gmail.com>
2022-05-06 14:51:15 +0000

    removed some models, moved some to a better location and started setting up the form to send user registration to the sql server

--- BELL/checkout/models.py
+++ BELL/checkout/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Purchase(models.Model):
+    itemid = models.IntegerField()
+    sellerid = models.IntegerField()
+    buyerid = models.IntegerField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.itemid} {self.sellerid} {self.buyerid} {self.price}"
--- BELL/frontpage/models.py
+++ BELL/frontpage/models.py
@@ -6,28 +6,32 @@ class Locations(models.Model):
     zip = models.IntegerField(unique=True)
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f"{self.name} {self.zip}"
+
 
 class People(models.Model):
     pname = models.CharField(max_length=255)
     location = models.ForeignKey(Locations, default=0, on_delete=models.SET_DEFAULT)
 
-
-class Users(models.Model):
-    uuid = models.ForeignKey(People, on_delete=models.CASCADE)
-    user_name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255, blank=True)
-    password = models.CharField(max_length=9999, blank=True)
+    def __str__(self):
+        return f"{self.pname} {self.location}"
 
 
 class ItemImage(models.Model):
     image = models.CharField(max_length=9999, blank=False,default=0)
 
+    def __str__(self):
+        return f"{self.image}"
+
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
+    userid = models.IntegerField()
     user_rank = models.CharField(max_length=255, blank=True)
     starting_price = models.FloatField()
     first_image = models.ForeignKey(ItemImage, default=0, on_delete=models.SET_DEFAULT)
     images = ArrayField(models.IntegerField())
+
 
 
