494a768e6d64cac3597959bc158fd04e6ec44395
ezithau <ezithau@gmail.com>
2022-05-12 16:34:26 +0000

    ratings enn í vinnslu

--- BELL/sellitem/models.py
+++ BELL/sellitem/models.py
@@ -18,24 +18,18 @@ storage = GoogleCloudStorage()"""
 
 
 class ListItem(models.Model):
-    poster = models.ForeignKey(User, on_delete=models.CASCADE)
     itemname = models.CharField(max_length=255)
     condition = models.CharField(max_length=20)
     description = models.CharField(max_length=1000)
     base_price = models.FloatField(default=0)
     main_image = models.CharField(max_length=9999, null=True, blank=True)
-    image2 = models.CharField(max_length=9999,  null=True, blank=True)
+    image2 = models.CharField(max_length=9999, null=True, blank=True)
     image3 = models.CharField(max_length=9999, null=True, blank=True)
     image4 = models.CharField(max_length=9999, null=True, blank=True)
     image5 = models.CharField(max_length=9999, null=True, blank=True)
-    itemurl = models.SlugField(max_length=100, unique=True)
-
-    def _get_unique_url(self, *args, **kwargs):
-        self.itemurl = slugify(self.itemname)
-        super(ListItem, self).save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.itemname} {self.poster} {self.condition} {self.description} {self.itemurl} {self.tag}"
+        return f"{self.itemname}  {self.condition} {self.description} {self.tag}"
 
     def __float__(self):
         return self.base_price
