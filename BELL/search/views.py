bf5b3faa63a03b9557f740d7d0ecde2c3cb5264c
Captain Iceboy <richard21@ru.is>
2022-05-13 09:46:22 +0000

    Sorting for search works

--- BELL/search/views.py
+++ BELL/search/views.py
@@ -6,8 +6,16 @@ from sellitem.models import ListItem
 def index(request):
     if request.method == "POST":
         searched = request.POST['searched']
-        items = ListItem.objects.filter(itemname__icontains=searched)
-        return render(request, 'search/index.html', {'searched': searched, 'items': items})
+        sorting = request.POST['sorting']
+        if sorting == 'name':
+            items = ListItem.objects.filter(itemname__icontains=searched).order_by('itemname')
+            return render(request, 'search/index.html', {'searched': searched, 'items': items})
+        elif sorting == 'lower-price':
+            items = ListItem.objects.filter(itemname__icontains=searched).order_by('base_price')
+            return render(request, 'search/index.html', {'searched': searched, 'items': items})
+        elif sorting == 'higher-price':
+            items = ListItem.objects.filter(itemname__icontains=searched).order_by('-base_price')
+            return render(request, 'search/index.html', {'searched': searched, 'items': items})
     else:
         return render(request, 'search/index.html', {})
 
