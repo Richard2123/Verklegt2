73567fe3f6e362c981b01cb773d386c0f6b728ed
Captain Iceboy <richard21@ru.is>
2022-05-13 09:14:00 +0000

    Added search functionallity

--- BELL/search/views.py
+++ BELL/search/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from sellitem.models import ListItem
 
 
 # Create your views here.
 def index(request):
     if request.method == "POST":
         searched = request.POST['searched']
-        return render(request, 'search/index.html', {'searched': searched})
+        items = ListItem.objects.filter(itemname__icontains=searched)
+        return render(request, 'search/index.html', {'searched': searched, 'items': items})
     else:
-        return render(request, 'search/index.html')
+        return render(request, 'search/index.html', {})
+
+
 
