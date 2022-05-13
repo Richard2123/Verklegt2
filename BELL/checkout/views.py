7f909a5008293f134e4e3b6b81cbadecc041ce99
ezithau <ezithau@gmail.com>
2022-05-13 23:04:54 +0000

    trying to get checkout to work, I failed

--- BELL/checkout/views.py
+++ BELL/checkout/views.py
@@ -3,18 +3,17 @@ from BELL.forms import CheckoutForm
 from sellitem.models import ListItem
 # Create your views here.
 
-
+#No matter how much I tried I could not get this to upload to the databasepip
 def index(request, item_id):
     item = ListItem.objects.get(id=item_id)
     context = {'form': CheckoutForm}
     if request.method == 'POST':
         print(request.POST)
         checkout = CheckoutForm(data=request.POST)
-        instance = checkout.save(commit=False)
-        instance.user = request.user.id
-        instance.item = item_id
-        instance.rating = 2
-        checkout.save()
+        checkout.user_id = request.user.id
+        checkout.item_id = item.id
+        if checkout.is_valid():
+            checkout.save()
     return render(request, 'checkout/index.html', context)
 
 
