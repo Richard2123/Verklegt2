c09efb6965591fad0c8bb4fcefd77d8470eaa4f9
ezithau <ezithau@gmail.com>
2022-05-13 22:07:27 +0000

    trying to disable bidding on sold items

--- BELL/checkout/urls.py
+++ BELL/checkout/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('checkout/<int:itemd_id>/', views.index, name="checkout-index"),
+    path('<int:item_id>/', views.index, name="checkout-index"),
 ]
