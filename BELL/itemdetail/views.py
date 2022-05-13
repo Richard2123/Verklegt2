bc6f59266551e4c197ad9ecb54997f65129b718d
ezithau <ezithau@gmail.com>
2022-05-13 13:57:14 +0000

    connected your items to show your item listings

--- BELL/itemdetail/views.py
+++ BELL/itemdetail/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from sellitem.models import get_specific_item
+from sellitem.models import ListItem
 
 
-def get_itemdetails(itemid):
-    return get_specific_item(itemid)
+"""def get_itemdetails(item_id=0):
+    return get_specific_item(item_id)"""
 
 
-def index(request, itemid):
-    return render(request, 'itemdetails/index.html', context={'itemdetails': get_itemdetails(itemid)})
+def index(request, item_id):
+    item = ListItem.objects.get(item_id)
+    return render(request, 'itemdetails/index.html', context={'itemdetails': item})
 
